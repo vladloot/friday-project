c65e7007dd6063cd484e6c7a8f0c9dcd213c644b
Agitobl <vlad.agito@gmail.com>
2022-02-02 16:57:54 +0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import './App.css';
 import {Login} from './components/Login/Login';
 import {SuperComponents} from "./components/SuperComponents/SuperComponents";
+import {Route, Routes} from "react-router-dom";
+import {NewPassword} from "components/NewPassword/NewPassword";
+import {NotFoundPage} from "components/NotFoundPage/NotFoundPage";
+import {PasswordRecovery} from "components/PasswordRecovery/PasswordRecovery";
+import {Profile} from "components/Profile/Profile";
+import {Registration} from "components/Registration/Registration";
 
 const App = () => {
     return (
         <div className="App">
-            <Login/>
-            <SuperComponents/>
+            <Routes>
+                <Route path="login" element={<Login/>}/>
+                <Route path="password" element={<NewPassword/>}/>
+                <Route path="404" element={<NotFoundPage/>}/>
+                <Route path="password-recovery" element={<PasswordRecovery/>}/>
+                <Route path="profile" element={<Profile/>}/>
+                <Route path="registration" element={<Registration/>}/>
+                <Route path="super-components" element={<SuperComponents/>}/>
+            </Routes>
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
 import {store} from './store/store';
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
 
-  </React.StrictMode>,
-  document.getElementById('root')
+    </React.StrictMode>,
+    document.getElementById('root')
 );
