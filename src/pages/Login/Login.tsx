1d872b3f2625320c9af6e4a59edf2595542f1b3a
Andrei <yantsevich92@gmail.com>
2022-02-06 23:59:20 +0300

    created UI Login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,23 @@
 import React, { FC } from 'react';
 
-const Login: FC = () => <div>Login</div>;
+import { Link } from 'react-router-dom';
+
+import Button from 'components/Button/Button';
+import Input from 'components/Input/Input';
+
+const Login: FC = () => (
+  <div>
+    <h1>FlashCards</h1>
+    <h2>Sign In</h2>
+    <form>
+      <Input type="text" placeholder="Email" />
+      <Input type="password" placeholder="Password" />
+      <Link to="/password-recovery">Forgot Password</Link>
+      <Button>Login</Button>
+      <span>Don&apos;t have an account?</span>
+      <Link to="/registration">Sign Up</Link>
+    </form>
+  </div>
+);
 
 export default Login;
