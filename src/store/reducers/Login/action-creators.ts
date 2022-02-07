e9c2e75eaafafe8a6b3dabe9fa311a8bf4a29fba
Andrei <yantsevich92@gmail.com>
2022-02-07 17:39:27 +0300

    added error handle and loading control to login flow

--- src/store/reducers/Login/action-creators.ts
+++ src/store/reducers/Login/action-creators.ts
@@ -1,16 +1,21 @@
 import { LoginParamsType, LoginService } from 'api/LoginService';
+import { AppActionCreators } from 'store/reducers/App/action-creators';
 import { LoginActionEnum } from 'store/reducers/Login/types';
 import { AppDispatch } from 'store/store';
+import { handleError } from 'utils/error-utils';
 
 export const LoginActionCreators = {
   setIsLoggedIn: (isLoggedIn: boolean) =>
     ({ type: LoginActionEnum.SET_IS_LOGGED_IN, isLoggedIn } as const),
   login: (loginData: LoginParamsType) => async (dispatch: AppDispatch) => {
     try {
+      dispatch(AppActionCreators.setAppIsLoading(true));
       await LoginService.login(loginData);
       dispatch(LoginActionCreators.setIsLoggedIn(true));
-    } catch (e) {
-      console.log(e);
+    } catch (error) {
+      handleError(error, dispatch);
+    } finally {
+      dispatch(AppActionCreators.setAppIsLoading(false));
     }
   },
 };
--- src/store/reducers/Login/types.ts
+++ src/store/reducers/Login/types.ts
@@ -1,7 +1,7 @@
 import { LoginActionCreators } from 'store/reducers/Login/action-creators';
 
 export enum LoginActionEnum {
-  SET_IS_LOGGED_IN = 'auth/SET_IS_LOGGED_IN',
+  SET_IS_LOGGED_IN = 'login/SET_IS_LOGGED_IN',
 }
 
 export type LoginActions = ReturnType<typeof LoginActionCreators.setIsLoggedIn>;
