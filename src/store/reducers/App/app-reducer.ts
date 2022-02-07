c924c3706dd19b410f4952aa48885641763441e8
Andrei <yantsevich92@gmail.com>
2022-02-07 17:37:20 +0300

    created app bll

--- src/store/reducers/App/app-reducer.ts
+++ src/store/reducers/App/app-reducer.ts
@@ -1,10 +1,21 @@
-type AppState = {};
-type AppActions = any;
+import { AppActionEnum, AppActions } from 'store/reducers/App/types';
 
-const initialState: AppState = {};
+const initialState = {
+  isInitialized: false,
+  isLoading: false,
+  error: '',
+};
+
+type AppState = typeof initialState;
 
 export const appReducer = (state = initialState, action: AppActions): AppState => {
   switch (action.type) {
+    case AppActionEnum.SET_IS_INITIALIZED:
+      return { ...state, isInitialized: action.isInitialized };
+    case AppActionEnum.SET_IS_LOADING:
+      return { ...state, isLoading: action.isLoading };
+    case AppActionEnum.SET_ERROR:
+      return { ...state, error: action.error };
     default:
       return state;
   }
