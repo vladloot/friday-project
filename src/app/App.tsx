36bc328d2620cf66dcd6680ed8f7daa0e005d993
Moon <m.p.skuratova@gmail.com>
2022-02-07 13:37:26 +0600

    connected eslint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import {Provider} from 'react-redux';
 import {HashRouter} from 'react-router-dom';
 import {store} from '../store/store';
 import AppRouter from './AppRouter';
-import Header from "../components/Header/Header";
+import Header from '../components/Header/Header';
 import './App.css'
 
 const App = () => {
